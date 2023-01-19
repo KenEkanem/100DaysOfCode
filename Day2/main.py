0a711b62331c0f5276044b14c67588ed57209caf
kenekanem <kennedyekanem@gmail.com>
2023-01-19 18:56:39 +0100

    simpleP

--- Day2/main.py
+++ Day2/main.py
@@ -1,11 +1,11 @@
 
 # ADDING TWO DIGIT INPUTS TOGETHER
 
-# two_digit_number = input("Type a two Digit number \n")
-# first_digit = two_digit_number[0]
-# second_digit = two_digit_number[1]
-# number = int(first_digit) + int(second_digit)
-# print(number)
+two_digit_number = input("Type a two Digit number \n")
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+number = int(first_digit) + int(second_digit)
+print(number)
 
 
 
@@ -16,32 +16,31 @@
 # **
 # */
 # +-
-
 # print(3*3+3/3-3)
 
 # A SIMPLE BMI CALCULATOR
 
-# height = input("Enter your height in m:\n")
-# weight = input("Enter your weight in kg:\n")
-# bmi = int(weight) / float(height) ** 2
-# bmi_as_integer = int(bmi)
+height = input("Enter your height in m:\n")
+weight = input("Enter your weight in kg:\n")
+bmi = int(weight) / float(height) ** 2
+bmi_as_integer = int(bmi)
 
-# print("Your bmi is: " + str(bmi_as_integer))
+print("Your bmi is: " + str(bmi_as_integer))
 
 # NUMBER MANIPULATION AND F STRING IN PYTHON
 
 
 # CALCULATING TIME LEFT IN WEEKS AND MONTHS
-# age = input("What is yout age?: ")
-# age_int = int(age)
-# age_left = 90 - age_int
-# days_left = age_left * 365
-# weeks_left = age_left * 52
+age = input("What is yout age?: ")
+age_int = int(age)
+age_left = 90 - age_int
+days_left = age_left * 365
+weeks_left = age_left * 52
 
-# months_left = age_left * 12
-# # print(months_left)
+months_left = age_left * 12
+# print(months_left)
 
-# print(f"You have {days_left} days {weeks_left} weeks, and {months_left} months left")
+print(f"You have {days_left} days {weeks_left} weeks, and {months_left} months left")
 
 
 print("Welcome to the tip calculator")
