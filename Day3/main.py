f4dc37e5d7ffeec35d82fe3dd3ce1ec255652321
kenekanem <kennedyekanem@gmail.com>
2023-01-20 13:34:30 +0100

    conditional statements

--- Day3/main.py
+++ Day3/main.py
@@ -1,11 +1,21 @@
 # CONDITIONAL STATEMENTS - CONTROL FLO WITH IF ELSE AND CONDITIONAL OPERATORS
 
+# Examples 
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("Please input your height in cm"))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster")
+# else:
+#     print("Sorry! You have to grow taller befor you can ride")
 
 
-print("Welcome to the rollercoaster!")
-height = int(input("Please input your height in cm"))
 
-if height >= 120:
-    print("You can ride the rollercoaster")
+number = int(input("Which number do you want to check?"))
+mod = number % 2
+
+if mod == 0:
+    print("This is an even number")
 else:
-    print("Sorry! You have to grow taller befor you can ride")
+    print("This is an odd number")
