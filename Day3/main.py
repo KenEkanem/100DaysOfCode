c005d428b94726dec8e822fa843569bf11d9b009
kenekanem <kennedyekanem@gmail.com>
2023-01-20 13:14:03 +0100

    Day3

---
+++ Day3/main.py
@@ -0,0 +1,11 @@
+# CONDITIONAL STATEMENTS - CONTROL FLO WITH IF ELSE AND CONDITIONAL OPERATORS
+
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("Please input your height in cm"))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry! You have to grow taller befor you can ride")
