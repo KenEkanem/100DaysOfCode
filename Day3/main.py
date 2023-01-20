ebc3e74704562ed51dafc7fce3640dc68538fda7
kenekanem <kennedyekanem@gmail.com>
2023-01-20 13:38:31 +0100

    oddEvenSh

--- Day3/main.py
+++ Day3/main.py
@@ -13,9 +13,10 @@
 
 
 number = int(input("Which number do you want to check?"))
-mod = number % 2
 
-if mod == 0:
+if number % 2 == 0:
     print("This is an even number")
 else:
     print("This is an odd number")
+
+
