763464c5bcc2adebab07b47b8e1bc2649f1433fe
kenekanem <kennedyekanem@gmail.com>
2023-01-24 14:16:35 +0100

    randomizing an iten in a list

---
+++ Day4/bankerroulette.py
@@ -0,0 +1,17 @@
+import random
+
+test_seed = int(input("Please input a seed number: "))
+random.seed(test_seed)
+
+
+namesAsCSV = input("Give everybody's names, seperated by a comma: ")
+names = namesAsCSV.split(",")
+
+num_items = len(names)
+random_choice = random.randint(0, num_items -1)
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the nmeal today")
+
+
+
