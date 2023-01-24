98c4c3421ce2593afb3c3b6969d009f1044b17c8
kenekanem <kennedyekanem@gmail.com>
2023-01-24 14:21:40 +0100

    Added .choice function

--- Day4/bankerroulette.py
+++ Day4/bankerroulette.py
@@ -7,9 +7,10 @@ random.seed(test_seed)
 namesAsCSV = input("Give everybody's names, seperated by a comma: ")
 names = namesAsCSV.split(",")
 
-num_items = len(names)
-random_choice = random.randint(0, num_items -1)
-person_who_will_pay = names[random_choice]
+# num_items = len(names)
+# random_choice = random.randint(0, num_items -1)
+# person_who_will_pay = names[random_choice]
+person_who_will_pay = random.choice(names)
 
 print(person_who_will_pay + " is going to buy the nmeal today")
 
