30cdb9ee3723370bd7b9ce889c3856dad6e26a40
kenekanem <kennedyekanem@gmail.com>
2023-01-25 16:07:54 +0100

    highestscore, max function

--- Day5/highestScore.py
+++ Day5/highestScore.py
@@ -1,5 +1,11 @@
-students_scores = input("Input a list of students scores ")
-for n in range(0, len(students_scores)):
-    students_scores[n] = int(students_scores[n])
-print(students_scores)
+student_scores = input("Input a list of student scores seperated by a space ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
 
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+     highest_score = score
+print(f"The highest score is{highest_score}")
